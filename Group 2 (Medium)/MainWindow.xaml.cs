e13a14b3d605bf549e8463fef6d78c366442800b
Gigi77789 <itsgigi777@gmail.com>
2024-01-23 04:53:11 +0000

    New version1

--- Group 2 (Medium)/MainWindow.xaml.cs
+++ Group 2 (Medium)/MainWindow.xaml.cs
@@ -30,29 +30,45 @@ namespace Group_2__Medium_
         private void numberTextbox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = !int.TryParse(e.Text, out int result);
-        }
+        } /* if "int.TryParse" successfully turn the input into a int, it will return a ture, then"!int.TryParse" will be false
+           if 'e.Handled" is false, then the input will be showed in the textbox*/
 
         private void numberTextbox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (int.TryParse(numberTextbox.Text, out int ellipseCount)) //turn the number into int
+            if (int.TryParse(numberTextbox.Text, out int ellipseInput)) //turn the number into int
             {
-                DrawEllipses(ellipseCount); //if the number is int, then start this method
+                DrawEllipses(ellipseInput); //if the number is int, then start this method
             }
         }
 
-        private void DrawEllipses(int ellipseCount)
+        private void DrawEllipses(int ellipseInput)
         {
-            int ellipseIndex = 0; //Track the current position of the ellipse being processed in the loop
-            foreach (var child in myGrid.Children) // search every elements in Canvas
+            int ellipseNumber = 0; //start to deal with the first ellipse  
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements; 
+            foreach (var element in myGrid.Children) /* use Loop "foreach" to search every elements on Canvas
+                                                      https://www.tutorialsteacher.com/articles/foreach-loop-in-csharp
+                */
             {
-                if (child is Ellipse ellipse) // search that if the element is an ellipse or not
+                if (element is Ellipse ellipse) /* use "is" to determine that if the element is an ellipse or not,
+                                               if it is, then this selected ellipse will be saved in the variable "ellipse"
+                                               https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/pattern-matching*/
+
                 {
-                    ellipse.Visibility = ellipseIndex < ellipseCount ? Visibility.Visible : Visibility.Collapsed;
-                    /*deal with the visibility, use"?" to compare the number we input and the number of visible ellipses. 
-                     * If the input number is more than number of visiable ellipses,then show more ellipses. 
-                     * If the input number is less than number of visiable ellipses,then show mless ellipses.
-                     * In total,  the number of visible ellipses equals the number that we input  */
-                    ellipseIndex++; //prepare to deal with the next ellipse, be visible or collapsed
+                    if (ellipseNumber < ellipseInput)
+                    {
+                        ellipse.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        ellipse.Visibility = Visibility.Collapsed;
+                    }
+                    /*deal with the visibility of the ellipse in the variable "ellipse"
+                     * Use"if" to compare the number we input and the number of "ellipseIndex". 
+                     * If the input number is more than number of "ellipseIndex",then show this ellipse. 
+                     * If the input number is less than number of "ellipseIndex",then collapse thus ellipse.*/
+
+                    ellipseNumber++; //prepare to deal with the next ellipse, continue the loop
+                    /* The loop stops when the number of "ellipseIndex equals the number that we input  */
                 }
             }
         }
